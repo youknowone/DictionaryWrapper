6c020725a08fbe4b0b56842dcf09eb8571316d99
Jeong YunWon <jeong@youknowone.org>
2014-09-20 20:47:17 +0900

    travis

--- DictionaryWrapperTests/DictionaryWrapperTests.m
+++ DictionaryWrapperTests/DictionaryWrapperTests.m
@@ -126,11 +126,11 @@ void test(id self) {
     XCTAssertEqualObjects(wrapped._object[@"value"], @"my value");
 }
 
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
-}
+//- (void)testPerformanceExample {
+//    // This is an example of a performance test case.
+//    [self measureBlock:^{
+//        // Put the code you want to measure the time of here.
+//    }];
+//}
 
 @end
